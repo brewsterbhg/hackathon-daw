f2e5aac9bedadbe2e81f2e9d0167a4aeba66dc7a
Singithi Kandage <skandage@nuvelance.io>
2021-02-19 10:31:23 -0500

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Body from "../Body/Body";
+
+const App =() => {
+  return (
+    <div className="App">
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import "./Body.scss";
+
+const Body =() => {
+  return (
+    <div className="body">
+        <div className="body__controls">
+            <p>Piano Roll</p>
+        </div>
+        <div className="body__channels"></div>
+    </div>
+  );
+}
+
+export default Body;
